932f00231eb27767b25333ae3c4648d7488ecf7e
manimutyla <manimutyala888@gmail.com>
2020-05-06 16:00:53 +0530

    Add files via upload

---
+++ librarymanagementsystemhibernate/src/main/java/com/capgemini/librarymanagementsystemhibernate/exception/AdminException.java
@@ -0,0 +1,7 @@
+package com.capgemini.librarymanagementsystemhibernate.exception;
+
+public class AdminException extends RuntimeException{
+	public AdminException(String msg) {
+		super(msg);
+	}
+}
